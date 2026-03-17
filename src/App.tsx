9dd8dda46b776ebdd9dfc0845e249eb41f188176
Matheus <matheusmartinscia@gmail.com>
2026-03-16 21:56:59 -0300

    Feat: Adiciona Header ao App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 
 import './styles/theme.css';
 import './styles/global.css';
+import { Header } from './components/Header';
 
 export const App = () => {
 
   return (
     <>
+      <Header />
     </>
   );
 }
