716798970fd55915276b994fbecc6b9bc7396ff5
Matheus <matheusmartinscia@gmail.com>
2026-03-21 21:47:47 -0300

    Feat: Abstrai componentes para page Board

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
 
 import './styles/theme.css';
 import './styles/global.css';
-import { Header } from './components/Header';
-import { Steps } from './components/Steps';
-import { Footer } from './components/Footer';
+import { Board } from './pages/Board';
 
 export const App = () => {
 
   return (
-    <>
-      <div style={{minHeight: '99vh', marginTop: '1px', display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
-        <Header />
-
-        <Steps />
-
-        <Footer />
-      </div>
-    </>
+    <Board />
   );
 }
