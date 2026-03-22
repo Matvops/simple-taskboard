d3e263f3f0d1deb83586d4084d2b9ba91a4089b8
Matheus <matheusmartinscia@gmail.com>
2026-03-21 21:36:54 -0300

    Feat: Adiciona footer ao app

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,18 @@ import './styles/theme.css';
 import './styles/global.css';
 import { Header } from './components/Header';
 import { Steps } from './components/Steps';
+import { Footer } from './components/Footer';
 
 export const App = () => {
 
   return (
     <>
-      <div style={{minHeight: '99vh', marginTop: '1px'}}>
+      <div style={{minHeight: '99vh', marginTop: '1px', display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
         <Header />
 
         <Steps />
+
+        <Footer />
       </div>
     </>
   );
