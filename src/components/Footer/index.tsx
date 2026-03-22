26b22ca56fbd35bd7299773b66cf70831c6f2396
Matheus <matheusmartinscia@gmail.com>
2026-03-21 21:36:43 -0300

    Feat: Cria Footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+import style from './style.module.css';
+
+export const Footer = () => {
+
+  return (
+    <>
+      <footer className={style.footer}>
+        <p className={style.text}>Criado por <a href="#">Matvops</a></p>
+      </footer>
+    </>
+  );
+}
