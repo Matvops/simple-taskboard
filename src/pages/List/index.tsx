631130ea79a30f4d8dccc81e80b21b6c08b9511b
Matheus <matheusmartinscia@gmail.com>
2026-03-21 21:51:21 -0300

    Feat: Cria page List

---
+++ src/pages/List/index.tsx
@@ -0,0 +1,13 @@
+import { Table } from "../../components/Table";
+import { MainTemplate } from "../../templates/MainTemplate";
+
+export const List = () => {
+
+  return (
+    <>
+      <MainTemplate>
+        <Table />
+      </MainTemplate>
+    </>
+  );
+}
