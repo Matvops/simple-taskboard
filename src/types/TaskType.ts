4f1f875d34319ff38fa2fc45388feed8e7cb4a1a
Matheus <matheusmartinscia@gmail.com>
2026-03-28 17:50:21 -0300

    Feat: Cria TaskType

---
+++ src/types/TaskType.ts
@@ -0,0 +1,6 @@
+export type TaskType = {
+    name: string | null,
+    priority: 'lowPriority' | 'mediumPriority' | 'highPriority',
+    stage: 'toDo' | 'blocked' | 'working' | 'done'
+    createdAt: number | null
+}
