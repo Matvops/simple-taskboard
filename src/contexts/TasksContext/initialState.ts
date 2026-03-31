215db5117bd9d5bb55ee2bd0a9679477ba15accb
Matheus <matheusmartinscia@gmail.com>
2026-03-30 22:05:08 -0300

    Feat: Remove mock do initialState das tasks do contexto

--- src/contexts/TasksContext/initialState.ts
+++ src/contexts/TasksContext/initialState.ts
@@ -1,12 +1,5 @@
 import type { TasksType } from "../../types/TasksTypes";
 
 export const initialState: TasksType = {
-   tasks: [
-    {
-        name: 'Teste 11111111',
-        priority: 'lowPriority',
-        stage: 'toDo',
-        createdAt: 0
-    }
-   ]
+   tasks: []
 }
