5fa0bccdfa0c67692448a2bfe9a863e2c1b628f8
Matheus <matheusmartinscia@gmail.com>
2026-03-30 22:00:06 -0300

    Feat: Remove null do type do atributo createdAt

--- src/types/TaskType.ts
+++ src/types/TaskType.ts
@@ -2,5 +2,5 @@ export type TaskType = {
     name: string | null,
     priority: 'lowPriority' | 'mediumPriority' | 'highPriority',
     stage: 'toDo' | 'blocked' | 'working' | 'done'
-    createdAt: number | null
+    createdAt: number
 }
