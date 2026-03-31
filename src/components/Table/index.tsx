e8c5982205ceb797505e979e4c611a794b60fcd3
Matheus <matheusmartinscia@gmail.com>
2026-03-31 20:44:35 -0300

    Feat: Adiciona retorno de tasks, na tabela

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,13 +1,53 @@
+import { useContext } from 'react';
 import style from './style.module.css';
+import { TaskContext } from '../../contexts/TasksContext';
+import { Utils } from '../../utils/Utils';
 
 export const Table = () => {
+
+  const { state } = useContext(TaskContext);
+
+  const prioritys = {
+    lowPriority: 'Baixa',
+    mediumPriority: 'Média',
+    highPriority: 'Alta',
+  }
+
+
+  const stages = {
+    toDo: 'A Fazer',
+    blocked: 'Bloqueado',
+    working: 'Fazendo',
+    done: 'Concluído',
+  }
+
+  const returnTasks = () => {
+
+    return state.tasks?.map((task, key) => {
+
+      return (
+        <>
+          <tr className={style.tableRow} key={key}>
+            <td className={`${style.tableData} ${style.description}`}>{task.name}</td>
+            <td className={`${style.tableData} ${style.date}`}>{Utils.numberToDate(task.createdAt)}</td>
+            <td className={style.tableData}>
+              <span className={style[task.stage]}>{stages[task.stage]}</span>
+            </td>
+            <td className={`${style.tableData}`}>
+              <span className={style[task.priority]}>&bull; {prioritys[task.priority]}</span>
+            </td>
+          </tr>
+        </>
+      )
+    });
+  }
 
   return (
     <>
       <div className={style.container}>
         <div className={style.headerContainer}>
           <h2 className={style.title}>Todas as tarefas</h2>
-          <span className={style.numberTasks}>8 tarefas</span>
+          <span className={style.numberTasks}>{state.tasks?.length} tarefas</span>
         </div>
 
         <table className={style.table}>
@@ -20,66 +60,7 @@ export const Table = () => {
             </tr>
           </thead>
           <tbody>
-            <tr className={style.tableRow}>
-              <td className={`${style.tableData} ${style.description}`}>Criar wireframes da interface principal</td>
-              <td className={`${style.tableData} ${style.date}`}>16/03/2026</td>
-              <td className={style.tableData}>
-                <span className={style.toDo}>A Fazer</span>
-              </td>
-              <td className={`${style.tableData}`}>
-                <span className={style.lowPriority}>&bull; Baixa</span>
-              </td>
-            </tr>
-            <tr className={style.tableRow}>
-              <td className={`${style.tableData} ${style.description}`}>Criar wireframes da interface principal</td>
-              <td className={`${style.tableData} ${style.date}`}>16/03/2026</td>
-              <td className={`${style.tableData}`}>
-                <span className={style.toDo}>A Fazer</span>
-              </td>
-              <td className={`${style.tableData}`}>
-                <span className={style.lowPriority}>&bull; Baixa</span>
-              </td>
-            </tr>
-            <tr className={style.tableRow}>
-              <td className={`${style.tableData} ${style.description}`}>Criar wireframes da interface principal</td>
-              <td className={`${style.tableData} ${style.date}`}>16/03/2026</td>
-              <td className={`${style.tableData}`}>
-                <span className={style.blocked}>Bloqueado</span>
-              </td>
-              <td className={`${style.tableData}`}>
-                <span className={style.mediumPriority}>&bull; Média</span>
-              </td>
-            </tr>
-            <tr className={style.tableRow}>
-              <td className={`${style.tableData} ${style.description}`}>Criar wireframes da interface principal</td>
-              <td className={`${style.tableData} ${style.date}`}>16/03/2026</td>
-              <td className={`${style.tableData}`}>
-                <span className={style.working}>Fazendo</span>
-              </td>
-              <td className={`${style.tableData}`}>
-                <span className={style.mediumPriority}>&bull; Média</span>
-              </td>
-            </tr>
-            <tr className={style.tableRow}>
-              <td className={`${style.tableData} ${style.description}`}>Criar wireframes da interface principal</td>
-              <td className={`${style.tableData} ${style.date}`}>16/03/2026</td>
-              <td className={`${style.tableData}`}>
-                <span className={style.working}>Fazendo</span>
-              </td>
-              <td className={`${style.tableData}`}>
-                <span className={style.highPriority}>&bull; Alta</span>
-              </td>
-            </tr>
-            <tr className={style.tableRow}>
-              <td className={`${style.tableData} ${style.description}`}>Criar wireframes da interface principal</td>
-              <td className={`${style.tableData} ${style.date}`}>16/03/2026</td>
-              <td className={`${style.tableData}`}>
-                <span className={style.done}>Concluído</span>
-              </td>
-              <td className={`${style.tableData}`}>
-                <span className={style.highPriority}>&bull; Alta</span>
-              </td>
-            </tr>
+            {returnTasks()}
           </tbody>
         </table>
       </div>
